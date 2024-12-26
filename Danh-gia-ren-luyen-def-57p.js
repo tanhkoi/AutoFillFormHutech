55c0438c90d5229df0a907b4f1cf80d073fb953c
Nguyễn Tấn Khôi <102349675+tanhkoi@users.noreply.github.com>
2024-12-26 20:44:12 +0700

    Create Danh-gia-ren-luyen-default-57pts.js

---
+++ Danh-gia-ren-luyen-def-57p.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelectorAll('input');
+const validIds = ['413', '416', '426', '429', '431', '434', '436', '440', '455', '470', '471', '472', '473', '491'];
+inputs.forEach(input => {
+    if (validIds.includes(input.id)) {
+        console.log(`Clicking input with ID: ${input.id}`);
+        input.click();
+    }
+});
