c39e2a25f75a48ae10f5c529589fc8fd1a4dbfaa
Nguyễn Tấn Khôi <102349675+tanhkoi@users.noreply.github.com>
2024-12-26 20:48:52 +0700

    Update and rename Danh-gia-ren-luyen-def-57p.js to Danh gia ren luyen.js

--- Danh gia ren luyen.js
+++ Danh gia ren luyen.js
@@ -1,3 +1,4 @@
+// Mở form và paste code vào console, 57 điểm mặc định
 const inputs = document.querySelectorAll('input');
 const validIds = ['413', '416', '426', '429', '431', '434', '436', '440', '455', '470', '471', '472', '473', '491'];
 inputs.forEach(input => {
